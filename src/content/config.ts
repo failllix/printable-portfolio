a4a807f5ee8585d0cf58972a14373f81f3a323ea
Felix Prünte <47022595+failllix@users.noreply.github.com>
2024-10-27 20:53:10 +0100

    Add certifications

---
+++ src/content/config.ts
@@ -0,0 +1,34 @@
+import { z, defineCollection } from "astro:content";
+
+const nameLink = z.object({
+  name: z.string(),
+  link: z.string(),
+});
+
+const projectsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    year: z.number(),
+    skills: z.array(z.string()),
+    coverImagePath: z.string(),
+    coverAlt: z.string(),
+    coCreators: z.array(nameLink),
+    excerpt: z.string(),
+    gitHubLink: z.string().optional(),
+  }),
+});
+
+const certificationsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    name: z.string(),
+    link: z.string(),
+    icon: z.enum(["🥇", "🥈", "🥉", "📃"]).default("📃"),
+  }),
+});
+
+export const collections = {
+  projects: projectsCollection,
+  certifications: certificationsCollection,
+};
